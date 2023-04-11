b87f2ea0bdb0f252bb5a99da0ac64ef64fa0aaab
Angel39706 <96604953+Angel39706@users.noreply.github.com>
2023-04-10 19:23:44 -0700

    Project files

---
+++ Campus Life/Campus Life/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Campus Life
+//
+//  Created by Angel Martinez on 4/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
