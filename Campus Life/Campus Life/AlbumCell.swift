abb180ade20e28e939fb929cc2acf4b5e0baea03
lxAnxietyxl <villatorokevin42@gmail.com>
2023-04-23 17:07:49 -0700

    adding missing cell

---
+++ Campus Life/Campus Life/AlbumCell.swift
@@ -0,0 +1,14 @@
+//
+//  AlbumCell.swift
+//  lab-tunley
+//
+//  Created by swft on 3/10/23.
+//
+
+import UIKit
+
+class AlbumCell: UICollectionViewCell {
+    
+    @IBOutlet weak var albumImageView: UIImageView!
+    
+}
